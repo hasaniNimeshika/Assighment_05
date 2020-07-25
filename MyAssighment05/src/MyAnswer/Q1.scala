f7aa25bef283892e44afe2cbfa2a90054f78e065
kanthi97 <65714939+kanthi97@users.noreply.github.com>
2020-07-25 23:23:31 +0530

    My first commit

---
+++ MyAssighment05/src/MyAnswer/Q1.scala
@@ -0,0 +1,20 @@
+package MyAnswer
+
+object Q1 extends App {
+   val r1 = new Rational(2,3)
+  println(r1)
+  println(r1.neg)
+  
+  
+
+}
+
+class Rational(n:Int,d:Int){
+  def numer=n
+  def denom=d
+ 
+  def neg = new Rational(-this.numer,this.denom)
+  
+  
+  override def toString= numer+"/"+denom
+}
